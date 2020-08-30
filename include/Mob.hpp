3cba8db42fbe93fefe8c9b1475bfd290cf489a26
Felix Morgner <felix.morgner@gmail.com>
2020-08-30 13:49:23 +0200

    mob: add per-frame collision shapes

--- include/Mob.hpp
+++ include/Mob.hpp
@@ -3,8 +3,13 @@
 
 #include "TypedNodeCastMixin.hpp"
 
+#include <AnimatedSprite.hpp>
+#include <CollisionPolygon2D.hpp>
 #include <Godot.hpp>
 #include <RigidBody2D.hpp>
+#include <String.hpp>
+
+#include <array>
 
 namespace dodgetc
 {
@@ -22,9 +27,18 @@ namespace dodgetc
     GODOT_CLASS(Mob, godot::RigidBody2D)
 
     auto on_screen_exited() -> void;
+    auto on_frame_changed() -> void;
+
+    auto set_collision_polygon(int frame) -> void;
 
     int minimum_speed{};
     int maximum_speed{};
+
+    godot::String type{};
+
+    godot::AnimatedSprite * sprite{};
+    godot::CollisionPolygon2D * active_collision_polygon{};
+    std::array<godot::CollisionPolygon2D *, 6> collision_polygons{};
   };
 
 }  // namespace dodgetc
--- source/Mob.cpp
+++ source/Mob.cpp
@@ -3,6 +3,7 @@
 #include "Rng.hpp"
 
 #include <AnimatedSprite.hpp>
+#include <CollisionPolygon2D.hpp>
 #include <SpriteFrames.hpp>
 
 namespace dodgetc
@@ -18,6 +19,8 @@ namespace dodgetc
   {
     godot::register_method("_ready", &Mob::_ready);
     godot::register_method("on_screen_exited", &Mob::on_screen_exited);
+    godot::register_method("on_frame_changed", &Mob::on_frame_changed);
+    godot::register_method("set_collision_polygon", &Mob::set_collision_polygon);
 
     godot::register_property("minimum_speed", &Mob::minimum_speed, default_minimum_speed);
     godot::register_property("maximum_speed", &Mob::maximum_speed, default_maximum_speed);
@@ -31,14 +34,44 @@ namespace dodgetc
 
   auto Mob::_ready() -> void
   {
-    auto animated_sprite = get_typed_node<godot::AnimatedSprite>("AnimatedSprite");
-    auto mob_types = animated_sprite->get_sprite_frames()->get_animation_names();
-    animated_sprite->set_animation(mob_types[random_int(0, mob_types.size() - 1)]);
+    sprite = get_typed_node<godot::AnimatedSprite>("AnimatedSprite");
+
+    collision_polygons[0] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameFly0");
+    collision_polygons[1] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameFly1");
+    collision_polygons[2] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameSwim0");
+    collision_polygons[3] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameSwim1");
+    collision_polygons[4] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameWalk0");
+    collision_polygons[5] = get_typed_node<godot::CollisionPolygon2D>("CollisionFrameWalk1");
+
+    auto mob_types = sprite->get_sprite_frames()->get_animation_names();
+    type = mob_types[random_int(0, mob_types.size() - 1)];
+    set_collision_polygon(0);
+    sprite->set_animation(type);
+  }
+
+  auto Mob::set_collision_polygon(int frame) -> void
+  {
+    auto offset = (type == "fly") ? 0 : (type == "swim") ? 2 : 4;
+
+    if (active_collision_polygon)
+    {
+      active_collision_polygon->set_disabled(true);
+      active_collision_polygon->set_visible(false);
+    }
+
+    active_collision_polygon = collision_polygons[frame + offset];
+    active_collision_polygon->set_disabled(false);
+    active_collision_polygon->set_visible(true);
   }
 
   auto Mob::on_screen_exited() -> void
   {
     queue_free();
+  }
+
+  auto Mob::on_frame_changed() -> void
+  {
+    call_deferred("set_collision_polygon", sprite->get_frame());
   }
 
 }  // namespace dodgetc
